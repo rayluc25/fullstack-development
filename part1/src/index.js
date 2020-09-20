5897312edcb3fed1071961fd7c51456647924b7c
Raymond Luc <rtluc@hotmail.com>
2020-09-20 11:17:44 -0700

    Initial commit of part 1

---
+++ part1/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+  return (
+    <div>
+      <p>Hello World, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
