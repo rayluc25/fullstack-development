24f4e98a6d53c852e420429cec98ad8ce825789b
Raymond Luc <rtluc@hotmail.com>
2020-09-22 21:23:23 -0700

    Made the changes described in Excercises 1.1-1.2.

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,20 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
+const Header = (props) => {
   return (
     <div>
-      <p>Hello World, it is {now.toString()}</p>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
       <p>
-        {a} plus {b} is {a + b}
+        {props.part} {props.exercises}
       </p>
     </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Content = (props) => {
+  return (
+    <div>
+      <Part part = {props.part1} exercises = {props.exercises1} />
+      <Part part = {props.part2} exercises = {props.exercises2} />
+      <Part part = {props.part3} exercises = {props.exercises3} />
+    </div>
+  )
+}
 
+const Total = (props) => {
+  return (
+    <div>
+      <p>
+        Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+      </p>
+    </div>
+  )
+}
 
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course = {course} />
+      <Content part1 = {part1} part2 = {part2} part3 = {part3}
+      exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3}
+      />
+      <Total exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
